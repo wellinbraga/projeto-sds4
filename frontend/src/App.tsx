3f338dde7f2fceeb002cb3ac923d7518cb1ff85f
Wellington Gilberto Braga <wellinbraga@gmail.com>
2021-09-12 20:28:29 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,38 +1,8 @@
-import BarChart from "components/BarChart";
-import DataTable from "components/DataTable";
-import DonutChart from "components/Dnutchart";
-import Footer from "components/Footer";
-import NavBar from "components/NavBar";
+import Routes from "Routes";
 
 function App() {
   return (
-    <>
-      <NavBar />
-      <div className="container">
-        <h1 className="text-primary py-3">Dashboard de Vendas</h1>
-
-        <div className="row px-3">
-          <div className="col-sm-6">
-            <h5 className="text-center text-secondary">Taxa de sucesso(%)</h5>
-            <BarChart />
-          </div>
-
-          <div className="col-sm-6">
-            <h5 className="text-center text-secondary">Taxa de sucesso(%)</h5>
-            <DonutChart />
-          </div>
-
-          <div  className="py-2">
-            <h2 className="text-primary">
-              Todas as Vendas
-            </h2>
-          </div>
-
-        </div>
-        <DataTable />
-      </div>
-      <Footer />
-    </>
+      <Routes />
   );
 }
 
